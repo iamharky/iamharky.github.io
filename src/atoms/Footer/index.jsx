35e44e254b35a02bab9a663f60147a27f174f06e
iamharky <harky@yandex.com>
2020-12-03 20:36:03 +0300

    Created Footer.

---
+++ src/atoms/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Copyright{' '}
+        <span role="img" aria-label="copyright">
+          ©️{' '}
+        </span>
+        Turgut Yavuz 2020
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
